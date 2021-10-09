29dc44c8c43368f0be0b9c3b70a952a7f58694cc
Aditya Jha <adityajha0102@gmail.com>
2021-10-09 19:47:07 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -8,7 +8,7 @@ import imdb
 
 def fetch_poster(movie_id):
     response = requests.get(
-        'https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(
+        'https://api.themoviedb.org/3/movie/{}?api_key=Provide your API key here 8&language=en-US'.format(
             movie_id))
     data = response.json()
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
